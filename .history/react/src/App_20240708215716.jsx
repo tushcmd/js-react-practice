b2b467f10e8eec840eca465d0d708ad9208e1de8
tushcmd <muturidavid854@gmail.com>
2024-07-08 22:05:06 +0300

    initial commit

---
+++ .history/react/src/App_20240708215716.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        <a href="https://tushdev.co" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ .history/react/src/App_20240708220153.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Simulating an API call
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name} - {user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
